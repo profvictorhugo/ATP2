1cb91e80c7125b5aba6dc282a832ea57a1d2aa7e
Victor Hugo Lopes <victorhullopes@gmail.com>
2026-05-02 21:29:31 -0300

    Alteração para melhoria no exerício

--- unidade7/listaU7Ex10.c
+++ unidade7/listaU7Ex10.c
@@ -4,6 +4,26 @@
 * exercício 10 da lista de exercícios da Unidade 07 - Bacharelado em Engenharia de Software IFG Inhumas-GO
 * Algoritmos e técnicas de programação 2 - ATP 2
 * prof. Dr. Victor Hugo Lopes
+* Enunciado resumido: leia os números de um sudoku
+* 9x9 e diga se está correto
+* Correto se:
+* 1) todas as linhas contendo 1 a 9
+* 2) todas as colunas contendo 1 a 9
+* 3) todos os quadrantes contendo 1 a 9
+*/
+/*
+Para executar, apenas cole o conteúdo no terminal. O jogo abaixo está correto!
+5 3 4 6 7 8 9 1 2
+6 7 2 1 9 5 3 4 8
+1 9 8 3 4 2 5 6 7
+8 5 9 7 6 1 4 2 3
+4 2 6 8 5 3 7 9 1
+7 1 3 9 2 4 8 5 6
+9 6 1 5 3 7 2 8 4
+2 8 7 4 1 9 6 3 5
+3 4 5 2 8 6 1 7 9
+
+
 */
 
 int checaLinhaColuna(int vetor[], int tam){
