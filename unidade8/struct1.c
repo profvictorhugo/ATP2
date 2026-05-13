926de612bb5d91ec0c85ec1914c71404ea27df97
Victor Hugo Lopes <victorhullopes@gmail.com>
2026-05-13 08:33:19 -0300

    Leitura de nome para struct

--- unidade8/struct1.c
+++ unidade8/struct1.c
@@ -13,5 +13,12 @@ struct Pessoa{
 int main(){
     struct Pessoa p1 = {"Victor Hugo Lopes", 43, 'm'};
     printf("Nome: %s\nIdade: %d\nSexo: %c\n\n", p1.nome, p1.idade, p1.sexo);
+    struct Pessoa p2;
+    printf("Informe o nome da pessoa: ");
+    fgets(p2.nome, 100, stdin);
+    printf("Informe a idade e sexo: ");
+    scanf("%d %c", &p2.idade, &p2.sexo);
+    printf("Nome: %s\nIdade: %d\nSexo: %c\n\n", p2.nome, p2.idade, p2.sexo);
+
     return 0;
 }
